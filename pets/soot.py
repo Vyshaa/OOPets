c8c431773e1c6bd1bce882f7787f6eef1d569edb
Vyshaa <achachin78@gmail.com>
2025-11-23 11:44:34 +0100

    Add mood's to soot

--- pets/soot.py
+++ pets/soot.py
@@ -4,7 +4,8 @@ class Soot(Animal):
     def __init__(self, name: str):
         super().__init__(name)
         self.image_folder_path = "pets/media/soot/"
-        self.action = "idle"
+        self.action = "neutral"
+        self.mood_list = ["angry", "sad", "neutral", "happy"]
         
     def speak(self):
         return "*Soot sprite noises*"
@@ -12,6 +13,9 @@ class Soot(Animal):
     def feed(self):
         self.action = "eat"
         super().feed
+        
+    def pet(self):
+        self.action = self.mood_list[self.get_mood()]
     
     def get_image_path(self):
         return self.image_folder_path + self.action + ".png" 
